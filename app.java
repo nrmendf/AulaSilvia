a24803f8205382b6d2204fdb97f74790fe26d2b7
Nilson Filho <104315653+nrmendf@users.noreply.github.com>
2023-03-21 21:39:57 -0300

    Add files via upload

---
+++ app.java
@@ -0,0 +1,17 @@
+package src;
+
+public class index {
+
+    public static void public static void main(String[] args) {
+        Mare mare = new Mare():
+
+        mare.tipo = "Segundo a altura & Segundo a fase da lua";
+        mare.coeficiente_de_Maré = "No espaço & No tempo";
+        mare.correntezas_de_Maré = "Circulação termohalina";
+        mare.deRio = "Pororoca (Amazonas)";
+        mare.doUniverso = "Terra";
+
+        System.out.printlf("As Marés se classificão em %s, %s, %s, %s, %s", mare.tipo, mare.coeficiente_de_Maré, mare.correntezas_de_Maré, deRio, mare.doUniverso);
+
+    }
+}
---
+++ artigo.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Mare {
+    public class Mare {
+        String tipo;
+        String coeficiente_de_Maré;
+        String correntezas_de_Maré;
+        String deRio;
+        String doUniverso;
+    }
+}
